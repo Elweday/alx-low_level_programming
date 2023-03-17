9f1476f1f1542da337309d18d1bea01a78225870
nomadnasser <mohamemdnh2864@gmail.com>
2023-03-16 23:45:21 -0700

    commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function or something
+ *
+ * Return: success code 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu  byte(s)", sizeof(char));
+	printf("Size of an int: %lu  byte(s)", sizeof(int));
+	printf("Size of a long int: %lu  byte(s)", sizeof(long int));
+	printf("Size of a long long: %lu  byte(s)", sizeof(long long));
+	printf("Size of a float: %lu  byte(s)", sizeof(float));
+	return (0);
+}
