1d90a6641e2468032d8d2209cdf42434649676ea
nomadnasser <mohamemdnh2864@gmail.com>
2023-03-17 09:17:02 -0700

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - headers goes there
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar((char) i);
+	}
+	for (i = 97; i <= 101; i++)
+	{
+		putchar((char) i);
+	}
+	putchar('\n');
+	return (0);
+}
