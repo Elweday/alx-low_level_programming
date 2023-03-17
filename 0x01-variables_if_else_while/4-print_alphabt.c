cba072c5ca37d6f376edf3965c57c599c0c4b4cc
nomadnasser <mohamemdnh2864@gmail.com>
2023-03-17 08:59:03 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - headers goes there
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i != 13 && i != 101)
+		putchar((char) i);
+	}
+	putchar('\n');
+	return (0);
+}
