67365dc5210f055dde7330c51d8e454ff520c4d2
nomadnasser <mohamemdnh2864@gmail.com>
2023-03-17 00:18:55 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    	const unsigned int msg_len = sizeof(msg) - 1;
-    	write(STDERR_FILENO, msg, msg_len);
+	const unsigned int msg_len = sizeof(msg) - 1;
+	write(STDERR_FILENO, msg, msg_len);
 	return (1);
 }
