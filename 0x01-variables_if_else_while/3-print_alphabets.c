24af36048f414508ef7725bb729ce96ea9fe97ee
nomadnasser <mohamemdnh2864@gmail.com>
2023-03-17 04:53:01 -0700

    commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-	for (int i = 97; i <= 122; i++)
+	int i;
+
+	for (i = 97; i <= 122; i++)
 	{
 		putchar((char) i);
 	}
-	for (int i = 65; i <= 90; i++)
+	for (i = 65; i <= 90; i++)
 	{
 		putchar((char) i);
 	}
