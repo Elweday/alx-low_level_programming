b6b30687246db47c68f4f14f18f84423bb24e1b0
Mohammed Nasser Helal <100801944+Elweday@users.noreply.github.com>
2023-03-21 15:05:53 +0200

    d

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
 */
 void print_alphabet(void)
 {
-	int i;
+	char i;
 
-	for(i = 97; i<=122; i++)
+	for(i = 'a'; i<='z'; i++)
 	{
-		putchar(i);
+		_putchar(i);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
