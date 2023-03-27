565ea663a8c83d8043aa017851c9dc57cf478df2
nomadnasser <mohamemdnh2864@gmail.com>
2023-03-27 05:01:56 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - the main function or something
+ * @s: string to find its length
+ * Return: success code 0
+ */
+
+int _strlen(char *s)
+{ 
+	int len; char c;
+	
+	len = 0;
+	while (*c != '\0')
+	{
+		len++;
+		c++;
+
+	}
+
+	return len;
+
+}
+
