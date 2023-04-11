57dbeb452cedb91625d6e30f6232488d2050c4cb
nomadnasser <mohammednh2864@gmail.com>
2023-04-11 22:54:49 +0200

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * create_array - prints the name of the program
  * @size: number of arguments
@@ -8,14 +7,12 @@
  *
  * Return: Always 0 (Success)
 */
-
- char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
-
 	if (size == 0 || str == NULL)
 		return (NULL);
 
