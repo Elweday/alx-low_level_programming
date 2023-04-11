1a44b31e82007467614088555a7ca5cc3eeba165
nomadnasser <mohammednh2864@gmail.com>
2023-04-11 21:03:28 +0200

    commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		printf("%d\n",stoi(argv[1]) * stoi(argv[2]));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
