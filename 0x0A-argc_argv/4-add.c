db11a9c09759be7e4d814b2c4833d1e561c4b593
nomadnasser <mohammednh2864@gmail.com>
2023-04-11 21:27:01 +0200

    commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
 
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
 /**
  * main - prints the name of the program
  * @argc: number of arguments
@@ -11,9 +54,16 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
+	int r = 0;
+	int i;
+	
+	if (argc > 1)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		for (i = 1; i < argc; i++)
+		{
+			r += _atoi(argv[i]);
+		}
+		printf("%d\n", r);
 	}
 	else
 	{
